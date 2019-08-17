14e1e35750b993b1e889745ec159adbe026d489d
be1amor <53439031+be1amor@users.noreply.github.com>
2019-08-17 17:21:04 +0300

    jquery

--- js/main.js
+++ js/main.js
@@ -1,13 +1,64 @@
+$(function() {
+
+  var header = $("#header"),
+      introH = $("#intro").innerHeight(),
+      scrollOffset = $(window).scrollTop();
+
+  checkScroll(scrollOffset);
+
+  $(window).on("scroll", function() {
+      scrollOffset = $(this).scrollTop();
+
+      checkScroll(scrollOffset);
+  });
+
+  function checkScroll(scrollOffset) {
+      if( scrollOffset >= introH ) {
+          header.addClass("fixed");
+      } else {
+          header.removeClass("fixed");
+      }
+  }
+
+
+
+  
+  $("[data-scroll]").on("click", function(event) {
+      event.preventDefault();
+
+      var $this = $(this),
+          blockId = $this.data('scroll'),
+          blockOffset = $(blockId).offset().top;
+
+      $("#nav a").removeClass("active");
+      $this.addClass("active");
+
+      $("html, body").animate({
+          scrollTop:  blockOffset
+      }, 500);
+  });
+
+
+  $("#nav_toggle").on("click", function(event) {
+      event.preventDefault();
+
+      $(this).toggleClass("active");
+      $("#nav").toggleClass("active");
+  });
+
+
+});
+
+
+
 var acc = document.getElementsByClassName("statti_header");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
+   
     this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
@@ -16,3 +67,5 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+
